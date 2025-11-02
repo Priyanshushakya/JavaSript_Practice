fafcc6e788624e5d0f539559e960d9a24be75c27
Priyanshushakya <priyanshuetw2072003@gmail.com>
2025-11-02 15:05:09 +0530

    understand the map method and this concept

---
+++ Map_Method/script.js
@@ -0,0 +1,14 @@
+const array = [2, 4, 6, 7, 8, 45, 90];
+
+// In js a map method is transform each element in an array
+
+const squareValue = array.map((num) => num * num)
+
+const double = array.map((num) => num * 2);
+
+console.log("Double:", double);
+
+
+// console.log("square:", doubleValue);
+
+
