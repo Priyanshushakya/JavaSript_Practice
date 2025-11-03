ca080da286f8598295c1028e4d8be344211e2a70
Priyanshushakya <priyanshuetw2072003@gmail.com>
2025-11-03 11:07:28 +0530

    learn Destructing

---
+++ Destructuring/script.js
@@ -0,0 +1,11 @@
+let person ={
+    firstName :"Priyanshu",
+    lastName : "Shakya",
+    age :21,
+};
+let {firstName, lastName,age} = person;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
